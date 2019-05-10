7a99b8627a8969afd2518c44e97751df26dac12f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-10 11:55:49 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -7,7 +7,7 @@ export default (state = [], action) => {
     case "REMOVE_QUOTE":
       return state.filter(quote=> quote.id !== action.quoteId);
 
-    case "UP_VOTE_QUOTE":
+    case "UPVOTE_QUOTE":
       return state.map(quote => quote.id == action.quoteId ? {...quote, votes: quote.votes+1} : quote);
 
     case "DOWN_VOTE_QUOTE":
