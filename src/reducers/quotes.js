4120cfe8a24a613c7d098b39600fff47d015d72f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-10 11:36:01 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -3,14 +3,24 @@ import uuid from 'uuid';
 export default (state = [], action) => {
   let idx;
   switch (action.type) {
-    case "ADD_AUTHOR":
+    case "ADD_QUOTE":
       return [...state, action.author];
 
-    case "REMOVE_AUTHOR":
+    case "REMOVE_QUOTE":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
-    case "ADD_BOOK":
+    case "UP_VOTE_QUOTE":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+
+    case "DOWN_VOTE_QUOTE":
       let existingAuthor = state.filter(
         author => author.authorName === action.book.authorName
       );
