f60a639e183548bc48a0d281b5c604012dfb1cef
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-10 11:48:12 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -10,14 +10,7 @@ export default (state = [], action) => {
       return state.filter(quote=> quote.id !== action.quoteId);
 
     case "UP_VOTE_QUOTE":
-      let existingAuthor = state.filter(
-        author => author.authorName === action.book.authorName
-      );
-      if (existingAuthor.length > 0) {
-        return state;
-      } else {
-        return [...state, { authorName: action.book.authorName, id: uuid() }];
-      }
+      return state.map(quote => quote.id == action.quoteId ? {...quote, vote: quote.vote+1} : quote);
 
     case "DOWN_VOTE_QUOTE":
       let existingAuthor = state.filter(
